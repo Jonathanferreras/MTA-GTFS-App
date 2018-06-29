867af728ac8751878ac3d54ec13b38ce5909c65c
jonathanferreras <jonathanferreras@icloud.com>
2018-06-29 14:51:10 -0400

    removed modules folder

--- public/src/modules/callApi.js
+++
@@ -1,5 +0,0 @@
-export const callApi = async (route) => {
-  let results = await fetch(route).then(res => res.json());
-
-  return results;
-};
--- public/src/modules/fetchTrainTrips.js
+++
@@ -1,6 +0,0 @@
-import { callApi } from './callApi';
-
-export const fetchTrainTrips = (train_line) => {
-  return callApi(`/api/trainTrips/${train_line}`);
-};
-
