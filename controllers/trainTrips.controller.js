b9fb6f0694a9731cb5e8658ef764480e9b93a05e
jonathanferreras <jonathanferreras@icloud.com>
2018-07-06 16:02:26 -0400

    implementing mongoose to incorporate mongodb

--- controllers/trainTrips.controller.js
+++ controllers/trainTrips.controller.js
@@ -5,7 +5,6 @@ const gtfs_config = require('../config/gtfsConfig');
 
 exports.getTrainTrips = (train_line) => {
   return new Promise((resolve, reject) => {
-
     let settings = {
       method: 'GET',
       url: `http://datamine.mta.info/mta_esi.php?key=${gtfs_config.key}`,
@@ -22,10 +21,11 @@ exports.getTrainTrips = (train_line) => {
 
       let feed = MtaGtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
       feed.entity.forEach((train) => {
-
         if (train.trip_update){
-          if (train.trip_update.trip.route_id == train_line){
-            results.push(train.trip_update);   
+          let trip_info = train.trip_update
+
+          if (trip_info.trip.route_id == train_line || train_line.toLowerCase() === 'all'){
+            results.push(trip_info);
           }
         } 
       });
--- routes/trainTrips.js
+++ routes/trainTrips.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const train_trips_controller = require('../controllers/trainTripsController');
-
 const router = express.Router();
 
-
-// router.get('/:train_route', async (req, res, next) => {
-//   let train_schedule = await ts_controller.getTrainSchedule(req);
-//   console.log(train_schedule);
-
-//   res.status(200).send(train_schedule);
-// })
+const train_trips_controller = require('../controllers/trainTrips.controller');
 
 router.get('/:train_line', async (req, res, next) => {
-  let train_schedule = await train_trips_controller.getTrainTrips(req.params.train_line);
+  let train_trips = await train_trips_controller.getTrainTrips(req.params.train_line);
 
-  res.status(200).send(train_schedule);
+  res.status(200).send(train_trips);
 })
 
 module.exports = router;
