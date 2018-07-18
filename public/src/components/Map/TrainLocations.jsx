442ff4bb774d86731bd08168dc51a3ddd622b256
jonathanferreras <jonathanferreras@icloud.com>
2018-07-18 12:16:27 -0400

    test

--- public/src/components/Map/TrainLocations.jsx
+++ public/src/components/Map/TrainLocations.jsx
@@ -2,19 +2,24 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { Marker } from "react-google-maps";
+import Delay  from 'react-delay';
 
 class TrainLocations extends Component {
+  componentDidMount(){
+
+  }
   render() {
     if(this.props.train_stops.length > 0){
       return (
         <Fragment>
           {
             this.props.train_stops.map((train_stop, index) => {
-              return <Marker 
-              key={ index.toString() } 
-              position={{ lat: parseFloat(train_stop.stop_lat), lng: parseFloat(train_stop.stop_lon) }}
-              // animation= { google.maps.Animation.BOUNCE }
-              />;
+              return <Delay key={ index.toString() }>
+                <Marker key={ index.toString() } 
+                position={{ lat: parseFloat(train_stop.stop_lat), lng: parseFloat(train_stop.stop_lon) }}
+                animation= { google.maps.Animation.DROP }
+                />
+              </Delay>;
             })
           }
         </Fragment>
