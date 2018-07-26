6959a34d95546a41eb4d21162d71fd9c3cd178b6
jonathanferreras <jonathanferreras@icloud.com>
2018-07-26 16:34:27 -0400

    updated build files

--- public/src/actions/index.js
+++
@@ -1,6 +0,0 @@
-export { fetchTrainRoutes } from './TrainRoutes';
-export { fetchSelectedTrainRoute } from './TrainRoute';
-export { fetchTrainTrips } from './TrainTrips';
-export { fetchTrainStops } from './TrainStops';
-export { setRouteDirection } from './TrainRouteDirection';
-export { fetchGoogleMapsApiKey } from './GoogleMapsApiKey'; 
